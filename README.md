6afcfda2d404a55b2cd028676bf37513e65ca8b6
Jordan Castelloe <jordancastelloe@Jordans-MacBook-Pro.local>
2019-08-19 07:22:28 -0400

    blocked out comments for event listeners

---
+++ README.md
@@ -0,0 +1,12 @@
+# Monday
+
+## Concepts Covered
+- Event Listeners
+
+## Resources
+1. [Event Listeners](https://github.com/NewForce-at-Mountwest/client-side-mastery/blob/master/book-2-the-neophyte/chapters/JS_EVENTS.md)
+
+## Things to work on during lab time
+As always, these exercises are optional and to help you practice. You can work on any of these at any point, or on something completely different.
+
+1. [Event Listeners](https://github.com/NewForce-at-Mountwest/client-side-mastery/blob/master/book-2-the-neophyte/chapters/JS_EVENTS.md)
---
+++ script.js
@@ -0,0 +1,41 @@
+// ------------ Lightning Exercise 1 -----------//
+// In your HTML, create three header elements (and h1, an h2, and an h3) and give them each a unique id
+// In your JavaScript file, select each element by its id.
+// Log each element to the console to make sure you selected it correctly.
+
+
+// ----------------------- CLICK EVENTS ------------------------------------ //
+// Create a button that says 'dark mode'
+// Add a click event that just console.logs something
+
+// ------------------- Lightning Exercise 2 -----------//
+// Add a click event listener to each heading element you just created.
+// Inside the event listener's callback function, you should log a string about which element they clicked on to the console
+
+// ------------------ classList.Add()-----------------//
+// Write some logic in the button's callback function to change the page to dark mode
+
+
+//----------------------- Lightning Exercise 3 ------------------//
+// Create a CSS file and link it to your HTML file
+// Inside the event listener for your h1 element, add a line of code that will change the text color of the h1 element to red when you click on it
+
+
+
+// --------------  OTHER TYPES OF EVENTS // FUNCTIONS OUTSIDE EVENT LISTENERS -------------------//
+// Add event listeners to each input to highlight the form fields
+// You want to reuse your logic, so define it in a function outside the event listener
+// let's also log the value of the text to the console
+
+
+//---------------------- Lightning Exercise 4------------------------//
+// Add a textarea element and a button with the text "Submit" to your HTML file
+// Give the button an id
+// Add a click event listener to the submit button
+// When clicked, it should log the value of the textarea to the console
+
+
+// ------------------------ SPA's ----------------------- //
+// Add click events to your nav bar
+// Each click event should print a string to the DOM
+// event bubbling-- select the entire body tag, 
